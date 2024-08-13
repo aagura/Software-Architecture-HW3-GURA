f01adbe6e31afb5507bcbcf0afa9343f4ad2b4e0
Александр Гура <sansayguru@gmail.com>
2024-08-13 14:40:29 +0300

    HW2 competed

---
+++ HW_3/Washing.java
@@ -0,0 +1,7 @@
+package HW_1.HW_3;
+
+public interface Washing {
+    void wash(Car car);
+
+
+}
---
+++ HW_3/WashingStation.java
@@ -0,0 +1,17 @@
+package HW_1.HW_3;
+
+public class WashingStation implements Washing{
+    @Override
+    public void wash(Car car) {
+                System.out.println("Помыли машину "+ car.getMake()+" "+car.getModel());
+
+            }
+
+
+
+
+
+
+
+
+}
